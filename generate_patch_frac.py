92a62a6d4aa6173308231632c61b20d4b20fea04
mdekauwe <mdekauwe@gmail.com>
2019-07-30 16:37:13 +1000

    Added boilerplate code

--- generate_patch_frac.py
+++ generate_patch_frac.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import xarray as xr
+import netCDF4 as nc
 import datetime
 import os
 import sys
@@ -44,6 +45,63 @@ def main():
     plt.colorbar()
     plt.show()
 
+    ncols = 841
+    nrows = 681
+
+    # create file and write global attributes
+    out_fname = "patch_frac.nc"
+    f = nc.Dataset(out_fname, 'w', format='NETCDF4')
+    f.description = 'patchfrac for CABLE, created by Martin De Kauwe'
+
+    f.history = "Created by: %s" % (os.path.basename(__file__))
+    f.creation_date = "%s" % (datetime.datetime.now())
+    f.contact = "mdekauwe@gmail.com"
+
+    # set dimensions
+    f.createDimension('time', None)
+    f.createDimension('z', 1)
+    f.createDimension('y', nrows)
+    f.createDimension('x', ncols)
+
+
+    z = f.createVariable('z', 'f8', ('z',))
+    z.long_name = "z"
+    z.long_name = "z dimension"
+
+    y = f.createVariable('y', 'f8', ('y',))
+    y.long_name = "y"
+    y.long_name = "y dimension"
+
+    x = f.createVariable('x', 'f8', ('x',))
+    x.long_name = "x"
+    x.long_name = "x dimension"
+
+    time = f.createVariable('time', 'f8', ('time',))
+    time.units = "seconds since 1999 00:00:00"
+    time.long_name = "time"
+    time.calendar = "standard"
+
+    latitude = f.createVariable('latitude', 'f8', ('y', 'x',))
+    latitude.units = "degrees_north"
+    latitude.missing_value = -999.
+    latitude.long_name = "Latitude"
+
+    longitude = f.createVariable('longitude', 'f8', ('y', 'x',))
+    longitude.units = "degrees_east"
+    longitude.missing_value = -999.
+    longitude.long_name = "Longitude"
+
+
+    # write data to file
+    x[:] = ncols
+    y[:] = nrows
+    z[:] = 1
+    time[:] = fper.time.values
+    latitude[:,:] = fper.latitude.values
+    longitude[:,:] = fper.longitude.values
+
+
+    f.close()
 
 if __name__ == "__main__":
 
