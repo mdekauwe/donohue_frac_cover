6f1c81d4855547b3cfe1a454feba3ef28ef9088b
mdekauwe <mdekauwe@gmail.com>
2019-07-30 15:40:17 +1000

    Added LAI

--- generate_netcdf_files.py
+++ generate_netcdf_files.py
@@ -22,13 +22,19 @@ import sys
 
 def main(files, var):
 
-    output_dir = "nc_files"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    print(var)
 
     meta = {'ncols': 841.0, 'cellsize': 0.05, 'nrows': 681.0,
             'xllcorner': 111.975, 'yllcorner': -44.025,
             'nodata_value': -999.0}
+
+    output_dir = "nc_files"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    output_dir = "nc_files/%s" % (var)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     for fname in files:
 
@@ -64,9 +70,14 @@ def main(files, var):
         x.long_name = "x"
         x.long_name = "x dimension"
 
-        year = fname[11:15]
-        month = fname[16:18]
-        day = fname[18:20]
+        if var == "lai":
+            year = fname[28:32]
+            month = fname[32:34]
+            day = fname[34:36]
+        else:
+            year = fname[11:15]
+            month = fname[16:18]
+            day = fname[18:20]
 
         n_timesteps = 1
         times = []
@@ -76,7 +87,7 @@ def main(files, var):
             secs += 1800.
 
         time = f.createVariable('time', 'f8', ('time',))
-        time.units = "seconds since %s 00:00:00" % (year)
+        time.units = "seconds since %s-%s-%s 00:00:00" % (year, month, day)
         time.long_name = "time"
         time.calendar = "standard"
 
@@ -99,10 +110,8 @@ def main(files, var):
             varx.long_name = "fraction cover"
         elif var == "frec":
             varx.long_name = "fraction recurrent component (grass)"
-
-        meta = {'ncols': 841.0, 'cellsize': 0.05, 'nrows': 681.0,
-                'xllcorner': 111.975, 'yllcorner': -44.025,
-                'nodata_value': -999.0}
+        elif var == "lai":
+            varx.long_name = "leaf area index"
 
         # 111.975 + (841. * 0.05)
         # -44.025 + (681. * 0.05)
@@ -136,13 +145,17 @@ def main(files, var):
 if __name__ == "__main__":
 
     var = "fper"
-    files = glob.glob("raw/*_%s_gimms3g_clim.flt" % (var))
+    files = glob.glob("raw/%s/*_%s_gimms3g_clim.flt" % (var, var))
     main(files, var)
 
     var = "fcov"
-    files = glob.glob("raw/*_%s_gimms3g_clim.flt" % (var))
+    files = glob.glob("raw/%s/*_%s_gimms3g_clim.flt" % (var, var))
     main(files, var)
 
     var = "frec"
-    files = glob.glob("raw/*_%s_gimms3g_clim.flt" % (var))
+    files = glob.glob("raw/%s/*_%s_gimms3g_clim.flt" % (var, var))
+    main(files, var)
+
+    var = "lai"
+    files = glob.glob("raw/%s/*.flt" % (var))
     main(files, var)
