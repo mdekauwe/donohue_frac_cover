0c077b9d300a4f80ee1fbcc913191f7b9de16a06
mdekauwe <mdekauwe@gmail.com>
2019-07-30 16:55:24 +1000

    Added further patch frac logic

--- generate_patch_frac.py
+++ generate_patch_frac.py
@@ -32,7 +32,8 @@ def main():
     tree = fper.fper[0,:,:]
     grass = frec.frec[0,:,:]
     bare = 1.0 - tree - grass
-
+    total = tree + grass + bare
+    empty = total * 0.0
 
     # Check sums to less than 1
     #total = frec.frec[0,:,:] + fper.fper[0,:,:]
@@ -41,12 +42,13 @@ def main():
     #plt.colorbar()
     #plt.show()
 
-    plt.imshow(bare)
-    plt.colorbar()
-    plt.show()
+    #plt.imshow(bare)
+    #plt.colorbar()
+    #plt.show()
 
     ncols = 841
     nrows = 681
+    npfts = 17 # n_tiles -> cable_define_types
 
     # create file and write global attributes
     out_fname = "patch_frac.nc"
@@ -58,15 +60,13 @@ def main():
     f.contact = "mdekauwe@gmail.com"
 
     # set dimensions
-    f.createDimension('time', None)
-    f.createDimension('z', 1)
+    f.createDimension('patch', npfts)
     f.createDimension('y', nrows)
     f.createDimension('x', ncols)
 
-
-    z = f.createVariable('z', 'f8', ('z',))
-    z.long_name = "z"
-    z.long_name = "z dimension"
+    patch = f.createVariable('patch', 'f8', ('patch',))
+    patch.long_name = "patch"
+    patch.long_name = "patch dimensions"
 
     y = f.createVariable('y', 'f8', ('y',))
     y.long_name = "y"
@@ -75,11 +75,6 @@ def main():
     x = f.createVariable('x', 'f8', ('x',))
     x.long_name = "x"
     x.long_name = "x dimension"
-
-    time = f.createVariable('time', 'f8', ('time',))
-    time.units = "seconds since 1999 00:00:00"
-    time.long_name = "time"
-    time.calendar = "standard"
 
     latitude = f.createVariable('latitude', 'f8', ('y', 'x',))
     latitude.units = "degrees_north"
@@ -91,15 +86,33 @@ def main():
     longitude.missing_value = -999.
     longitude.long_name = "Longitude"
 
+    patchfrac = f.createVariable("patchfrac", 'f8', ('patch', 'y', 'x',))
+    patchfrac.units = "[0-1]"
+    patchfrac.missing_value = -999.
+    patchfrac.long_name = "Patch frac"
 
     # write data to file
     x[:] = ncols
     y[:] = nrows
-    z[:] = 1
-    time[:] = fper.time.values
     latitude[:,:] = fper.latitude.values
     longitude[:,:] = fper.longitude.values
-
+    patchfrac[0,:,:] = empty   #  evergreen_needleleaf
+    patchfrac[1,:,:] = tree    #  evergreen_broadleaf
+    patchfrac[2,:,:] = empty   #  deciduous_needleleaf
+    patchfrac[3,:,:] = empty   #  deciduous_broadleaf
+    patchfrac[4,:,:] = empty   #  shrub
+    patchfrac[5,:,:] = grass   #  C3
+    patchfrac[6,:,:] = empty   #  C4
+    patchfrac[7,:,:] = empty   #  Tundra
+    patchfrac[8,:,:] = empty   #  C3 crops
+    patchfrac[9,:,:] = empty   #  C4 crops
+    patchfrac[10,:,:] = empty  #  wetland
+    patchfrac[11,:,:] = empty  #  empty
+    patchfrac[12,:,:] = empty  #  empty
+    patchfrac[13,:,:] = bare   #  barren
+    patchfrac[14,:,:] = empty  #  urban
+    patchfrac[15,:,:] = empty  #  lakes
+    patchfrac[16,:,:] = empty  #  ice
 
     f.close()
 
