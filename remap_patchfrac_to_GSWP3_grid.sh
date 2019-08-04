d90e95ed7b8bf6371b71d1f41875c62ac3ca4549
mdekauwe <mdekauwe@gmail.com>
2019-08-05 09:18:39 +1000

    Changed path for nci

--- remap_patchfrac_to_GSWP3_grid.sh
+++ remap_patchfrac_to_GSWP3_grid.sh
@@ -1,3 +1,3 @@
-cdo remapnn,~/Desktop/GSWP3.BC.Tair.3hrMap.1929.nc  \
+cdo remapnn,~/g/data1/wd9/MetForcing/Global/GSWP3_2017/Tair/GSWP3.BC.Tair.3hrMap.1929.nc  \
             nc_files/patch_frac/patch_frac.nc \
             nc_files/patch_frac/patch_frac_0.5.nc
