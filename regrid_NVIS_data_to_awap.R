6f61aa745fd82e3592937776499f9ec05c5859c1
mdekauwe <mdekauwe@gmail.com>
2019-10-01 16:49:53 +1000

    Fixed regridding

--- regrid_NVIS_data_to_awap.R
+++ regrid_NVIS_data_to_awap.R
@@ -4,4 +4,5 @@ veg <- brick("lai_climatology.nc")
 
 awap <- raster("/Users/mdekauwe/Desktop/AWAP.Tair.3hr.2000.nc")
 regrid <- resample(veg, awap, method="bilinear")
-writeRaster(regrid, "lai_climatology_AWAP_grid.nc", varname="LAI", overwrite=TRUE)
+writeRaster(regrid, "lai_climatology_AWAP_grid.nc", varname="LAI", zname="time",
+            overwrite=TRUE)
